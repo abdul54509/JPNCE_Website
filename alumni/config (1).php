85fa69a0440c1b684dbf2e757620df0414545ecc
MOHD ABDUL HAQ <abdulhaq2575@gmail.com>
2021-05-21 21:12:40 +0530

    Add files via upload

---
+++ alumni/config (1).php
@@ -0,0 +1,9 @@
+<?php
+$url='sql308.epizy.com';
+$username='epiz_27402084';
+$password='wWJqn94NlxX';
+$conn=mysqli_connect($url,$username,$password,"epiz_27402084_alumni");
+if(!$conn){
+ die('Could not Connect My Sql:' .mysql_error());
+}
+?>
